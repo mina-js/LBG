0bbc119b88cc1b5d55a9ae76d9d787f448f86b23
Aminata Sow <78930639+mina-js@users.noreply.github.com>
2024-03-18 16:20:45 -0400

    why are the legs moving without the head lolol

--- Scripts/Leg.cs
+++ Scripts/Leg.cs
@@ -1,32 +1,42 @@
 using Godot;
 using System;
 
-public class Leg : HingeJoint
+public class Leg : Spatial
 {
-    [Export]
-    public NodePath handPath;
-    [Export]
-    private float liftForce = 10;
+	[Export]
+	public NodePath handPath;
+	[Export]
+	private float liftForce = 10;
 
-    public RigidBody hand;
+	[Export]
+	public NodePath legShapePath;
+	private CollisionShape legShape;
 
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
+	public RigidBody hand;
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        hand = GetNode<RigidBody>(handPath);
-    }
+	// Declare member variables here. Examples:
+	// private int a = 2;
+	// private string b = "text";
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(float delta)
-    {
-        if (Input.IsActionPressed("lift"))
-        {
-            GD.Print("Lift");
-            hand.AddForce(Vector3.Up * liftForce, hand.Transform.origin);
-        }
-    }
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		hand = GetNode<RigidBody>(handPath);
+		legShape = GetNode<CollisionShape>(legShapePath);
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(float delta)
+	{
+		if (Input.IsActionPressed("lift"))
+		{
+			GD.Print("Lift");
+			hand.AddForce(Vector3.Up * liftForce, hand.Transform.origin);
+		}
+	}
+	public void ScaleLeg(float scale)
+	{
+		GD.Print("Scaling leg " + Name + " to " + scale);
+		legShape.Scale = new Vector3(scale, 1, 1);
+	}
 }
--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -1,38 +1,42 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 
 public class Player : Spatial
 {
     [Export]
     public NodePath headNode;
+    [Export]
+    List<NodePath> legPaths;
 
     [Export]
     public int rotationSpeed = 5;
-
     [Export]
-    public int movementSpeed = 100;
+    public int impulseMultiplier = 2500;
 
     private Vector3 _targetVelocity = Vector3.Zero;
-    private KinematicBody headKB;
+    private RigidBody headRB;
+    private List<Leg> legs = new List<Leg>();
 
     public override void _Ready()
     {
-        headKB = GetNodeOrNull<KinematicBody>(headNode);
+        headRB = GetNodeOrNull<RigidBody>(headNode);
+        foreach (var legPath in legPaths)
+        {
+            legs.Add(GetNodeOrNull<Leg>(legPath));
+        }
     }
 
-    public override void _PhysicsProcess(float delta)
+    public override void _Process(float delta)
     {
-        Vector3 direction = headKB.Transform.basis.z;
-
-        if (Input.IsActionPressed("move_forward"))
+        if (Input.IsActionJustPressed("move_forward"))
         {
-            direction *= movementSpeed;
+            MakeMove(delta, new Vector2(1, 1));
         }
-
-        if (Input.IsActionPressed("move_backward"))
+        else if (Input.IsActionJustPressed("move_backward"))
         {
-            direction *= -1 * movementSpeed;
+            MakeMove(delta, new Vector2(-1, -1));
         }
 
         float spin = 0;
@@ -46,9 +50,71 @@ public class Player : Spatial
             spin += 1f;
         }
 
-        headKB.Rotate(Vector3.Up, spin * rotationSpeed * delta);
-        _targetVelocity = direction * delta;
-
-        headKB.MoveAndSlide(_targetVelocity);
+        headRB.Rotate(Vector3.Up, spin * rotationSpeed * delta);
     }
+
+    void MakeMove(float delta, Vector2 goalVector)
+    {
+        GD.Print("STARTING MOVE----");
+        GD.Print("goalVector: " + goalVector);
+
+        var randomlyReorderedLegs = new List<Leg>(legs.GetRange(3, 5));
+        Utils.Shuffle(randomlyReorderedLegs);
+
+        for (var i = 0; i < randomlyReorderedLegs.Count; i++)
+        {
+            randomlyReorderedLegs[i].ScaleLeg(1f + (i * 2f));
+        }
+
+        //Just apply the impulse in the desired direction, simplest
+        Vector3 impulse = new Vector3(goalVector.x, 0, goalVector.y) * impulseMultiplier * delta;
+        GD.Print("SIMPLE IMPULSE: " + impulse);
+        headRB.ApplyImpulse(
+            new Vector3(0, -1, 0),
+            impulse
+        );
+
+
+        // var collidingBodies = headRB.GetCollidingBodies();
+        // GD.Print("Colliding with: " + collidingBodies.Count);
+        // StaticBody floor = collidingBodies[0] as StaticBody;
+        // floor
+
+        //Apply the movement via KB
+        // headKB.MoveAndSlide(
+        //     new Vector3(goalVector.x, 0, goalVector.y) * movementSpeed * delta
+        // );
+
+    }
+
+    // public override void _PhysicsProcess(float delta)
+    // {
+    //     Vector3 direction = headKB.Transform.basis.z;
+
+    //     if (Input.IsActionPressed("move_forward"))
+    //     {
+    //         direction *= movementSpeed;
+    //     }
+
+    //     if (Input.IsActionPressed("move_backward"))
+    //     {
+    //         direction *= -1 * movementSpeed;
+    //     }
+
+    //     float spin = 0;
+
+    //     if (Input.IsActionPressed("rotate_clockwise"))
+    //     {
+    //         spin -= 1f;
+    //     }
+    //     else if (Input.IsActionPressed("rotate_counter_clockwise"))
+    //     {
+    //         spin += 1f;
+    //     }
+
+    //     headKB.Rotate(Vector3.Up, spin * rotationSpeed * delta);
+    //     _targetVelocity = direction * delta;
+
+    //     headKB.MoveAndSlide(_targetVelocity);
+    // }
 }
